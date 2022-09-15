1c633c4d06fe2b5ca134f80f9e13939c79f9d18d
fahad <lxtalgames@gmail.com>
2022-09-15 20:38:44 +0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,9 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          FAHAD
+
+          <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
